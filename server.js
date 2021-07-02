208039ee643d9019c3ecdf583bac50ebc9758a2b
Mehul Shekhada <mehulshekhada96@gmail.com>
2021-07-02 10:20:18 +0530

    Finished Code

--- server.js
+++ server.js
@@ -6,8 +6,7 @@ require('dotenv').config();
 var express = require('express');
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -19,6 +18,26 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+const requestIp = require('request-ip');
+// inside middleware handler
+var ipMiddleware = function(req, res, next) {
+ const clientIp = requestIp.getClientIp(req); 
+ next();
+};
+//As Connect Middleware
+app.use(requestIp.mw())
+
+
+app.get('/api/whoami', (req, res) => {
+  var ipadress = req.clientIp;
+  var language = req.acceptsLanguages();
+  var software=req.get('User-Agent');
+   res.json({
+   ipaddress: ipadress,
+   language:language[0],
+   software:software
+   });
+  });
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
